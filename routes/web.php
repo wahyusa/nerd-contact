3da51041e7493cd9aa07259db2af355d5eb2136d
wahyusa <27491616+wahyusa@users.noreply.github.com>
2025-07-06 00:29:59 +0700

    feat: contacts prototype works in JSON format no UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +11,17 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// JSON API routes for prototyping
+Route::prefix('api')->group(function () {
+    Route::get('contacts', [ContactController::class, 'index']);
+    Route::get('contacts/{contact}', [ContactController::class, 'show']);
+    Route::post('contacts', [ContactController::class, 'store']);
+    Route::put('contacts/{contact}', [ContactController::class, 'update']);
+    Route::delete('contacts/{contact}', [ContactController::class, 'destroy']);
+    Route::get('zodiacs', [ContactController::class, 'zodiacs']);
+    Route::get('contacts-stats', [ContactController::class, 'stats']);
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

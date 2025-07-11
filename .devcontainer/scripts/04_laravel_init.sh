858ddc38c6685ce2a2d1c9f36968c6d3a00e2fd9
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2025-07-11 08:22:19 +0000

    chore(devcontainer): fix xdebug warning

--- .devcontainer/scripts/04_laravel_init.sh
+++ .devcontainer/scripts/04_laravel_init.sh
@@ -6,6 +6,14 @@ if [ -z "$CODESPACES" ] && [ -z "$CODESPACE_NAME" ]; then
     exit 1
 fi
 
+echo "Installing composer dependencies..."
+composer install
+
+echo "Configuring Xdebug for Codespaces..."
+# Disable Xdebug step debugging by default to avoid connection warnings
+echo "xdebug.mode=develop,coverage" | sudo tee -a /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+echo "xdebug.start_with_request=no" | sudo tee -a /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
 echo "Running composer scripts for Laravel initial setup..."
 composer run post-root-package-install
 composer run post-create-project-cmd
